e2f57c44fb0722e0352c2c5cdc231e17aeefd055
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-08-23 15:33:13 +0530

    read yaml file using struct

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"strconv"
@@ -11,11 +12,50 @@ import (
 
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/joho/godotenv"
+	"gopkg.in/yaml.v3"
 )
 
-type Bucket struct {
-	DocCount int    `json:"doc_count"`
-	Key      string `json:"key"`
+type AlertConfig struct {
+	ElkHost      string   `yaml:"elk_host"`
+	ElkUsername  string   `yaml:"elk_username"`
+	ElkPassword  string   `yaml:"elk_password"`
+	ElkIndex     string   `yaml:"elk_index"`
+	ElkThreshold uint64   `yaml:"elk_threshold"`
+	Query        string   `yaml:"elk_query"`
+	Whitelist    []string `yaml:"whitelist"`
+}
+
+func ReadYaml(filepath string) *AlertConfig {
+	yamlFileReader, err := os.Open(filepath)
+	if err != nil {
+		log.Fatalf("Error Reading file: %s. Error:%s", filepath, err)
+		return &AlertConfig{}
+	}
+	defer yamlFileReader.Close()
+	// load yaml file
+	data, err := io.ReadAll(yamlFileReader)
+	if err != nil {
+		fmt.Println(err)
+		return &AlertConfig{}
+	}
+
+	// load config from yaml data
+	config := AlertConfig{}
+	err = yaml.Unmarshal([]byte(data), &config)
+	if err != nil {
+		log.Fatalf("Error:%s", err)
+		return &AlertConfig{}
+	}
+
+	// fmt.Println(config.ElkHost)
+	// fmt.Println(config.ElkUsername)
+	// fmt.Println(config.ElkPassword)
+	// fmt.Println(config.ElkIndex)
+	// fmt.Println(config.ElkThreshold)
+	// fmt.Println(config.Query)
+	// fmt.Println(config.Whitelist)
+
+	return &config
 }
 
 func main() {
@@ -24,6 +64,9 @@ func main() {
 	if err != nil {
 		log.Fatalf("Some error occured while loading .env file. Err: %s", err)
 	}
+
+	// Read the YAML file
+	ReadYaml("test.yaml")
 
 	// elk config
 	elkHost := os.Getenv("ELK_HOST")
